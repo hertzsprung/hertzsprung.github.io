be299004a928ee8f34e9be50347f39c5a58a9abd
James Shaw <js102@zepler.net>
2016-04-01 16:43:26 +0100

    add hamcrest-json and redis-launcher javadoc

---
+++ hamcrest-json/apidocs/assets/navtree_data.js
@@ -0,0 +1,8 @@
+var NAVTREE_DATA =
+[ [ "uk.co.datumedge.hamcrest.json", "uk/co/datumedge/hamcrest/json/package-summary.html", [ [ "Interfaces", null, [ [ "JSONComparator", "uk/co/datumedge/hamcrest/json/JSONComparator.html", null, "" ], [ "JSONModalComparator", "uk/co/datumedge/hamcrest/json/JSONModalComparator.html", null, "" ] ]
+, "" ], [ "Classes", null, [ [ "JSONComparisonResult", "uk/co/datumedge/hamcrest/json/JSONComparisonResult.html", null, "" ], [ "SameJSONAs", "uk/co/datumedge/hamcrest/json/SameJSONAs.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
---
+++ hamcrest-json/apidocs/reference/lists.js
@@ -0,0 +1,8 @@
+var DATA = [
+      { id:0, label:"uk.co.datumedge.hamcrest.json", link:"uk/co/datumedge/hamcrest/json/package-summary.html", type:"package" },
+      { id:1, label:"uk.co.datumedge.hamcrest.json.JSONComparator", link:"uk/co/datumedge/hamcrest/json/JSONComparator.html", type:"class" },
+      { id:2, label:"uk.co.datumedge.hamcrest.json.JSONComparisonResult", link:"uk/co/datumedge/hamcrest/json/JSONComparisonResult.html", type:"class" },
+      { id:3, label:"uk.co.datumedge.hamcrest.json.JSONModalComparator", link:"uk/co/datumedge/hamcrest/json/JSONModalComparator.html", type:"class" },
+      { id:4, label:"uk.co.datumedge.hamcrest.json.SameJSONAs", link:"uk/co/datumedge/hamcrest/json/SameJSONAs.html", type:"class" }
+
+    ];
