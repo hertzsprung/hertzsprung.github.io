a93e7f3c42b7954a111583649b0a338cdfa7f7c1
James Shaw <js102@zepler.net>
2016-04-05 14:35:36 +0100

    add autumn javadocs

---
+++ autumn/apidocs/assets/navtree_data.js
@@ -0,0 +1,8 @@
+var NAVTREE_DATA =
+[ [ "uk.co.datumedge.autumn", "uk/co/datumedge/autumn/package-summary.html", [ [ "Classes", null, [ [ "Singletoniser", "uk/co/datumedge/autumn/Singletoniser.html", null, "" ] ]
+, "" ], [ "Exceptions", null, [ [ "SingletoniseException", "uk/co/datumedge/autumn/SingletoniseException.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
