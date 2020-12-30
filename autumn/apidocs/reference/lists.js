f6ce4fc2a5f478a48286133dfe12a58690934330
James Shaw <js102@zepler.net>
2020-12-30 12:48:13 +0000

    remove unused detritus

--- autumn/apidocs/reference/lists.js
+++
@@ -1,6 +0,0 @@
-var DATA = [
-      { id:0, label:"uk.co.datumedge.autumn", link:"uk/co/datumedge/autumn/package-summary.html", type:"package" },
-      { id:1, label:"uk.co.datumedge.autumn.SingletoniseException", link:"uk/co/datumedge/autumn/SingletoniseException.html", type:"class" },
-      { id:2, label:"uk.co.datumedge.autumn.Singletoniser", link:"uk/co/datumedge/autumn/Singletoniser.html", type:"class" }
-
-    ];
